bf61fc5ee318ed711073e0b646e6fe70c1a7f88d
Pedro Santos <donbonifacio@gmail.com>
2014-10-26 16:35:36 +0000

    Update tests

--- test/obb_ranking/core_test.clj
+++ test/obb_ranking/core_test.clj
@@ -5,10 +5,9 @@
   (is calculate))
 
 (deftest calculate-ratings
-  (let [result (calculate {:rating 500 :rd 350 :volatility 40}
-                 {:rating 1500 :rd 350 :volatility 40})]
-    (println result)
-    (is result)))
+  (let [[p1 p2] (calculate {:rating 1500 :rd 50 :volatility 0.06}
+                           {:rating 500 :rd 350 :volatility 0.06})]
+    (is (> (:rating p1 1500)))))
 
 (deftest default-player-test
   (let [default (default-player)]
